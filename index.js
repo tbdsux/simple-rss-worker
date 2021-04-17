e14df57d7a9db200c0722eb3d1559becc435b148
TheBoringDude <iamcoderx@gmail.com>
2021-04-17 19:07:14 +0800

    update:: function capability

--- index.js
+++ index.js
@@ -1,33 +1,69 @@
+// XML parser
 const parser = require('fast-xml-parser')
 
-const fetcher = async () => {
-	return await fetch("http://rss.cnn.com/rss/cnn_latest.rss", {
-		method: "GET",
-	})
-		.then((r) => r.text())
-		.then((data) => data);
-};
+const fetcher = async (url) => {
+  return await fetch(url, {
+    method: 'GET',
+  })
+    .then((r) => r.text())
+    .then((data) => data)
+}
 
-
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
 /**
- * Respond with hello worker text
+ * Get the `url` in the request object.
+ * @param {Request} request
+ */
+const requestParser = async (request) => {
+  const { url } = await request.clone().json()
+
+  return url ? url : null
+}
+
+/**
+ * Respond with the parsed RSS JSON Data
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const r = await request.clone().json()
-  console.log(r)
+  // check if not POST
+  if (request.method !== 'POST') {
+    return new Response(
+      JSON.stringify({ error: 405, message: 'Method Not Allowed' }),
+      {
+        status: 405,
+        statusText: 'Method Not Allowed',
+      },
+    )
+  }
 
+  const rss_url = await requestParser(request)
 
-  let xmlData = await fetcher();
+  // `url` is not defined in request body
+  if (!rss_url) {
+    return new Response(
+      JSON.stringify(
+        {
+          error: '400',
+          message: 'Bad Request, please set the `url` in your body data.',
+        },
+        {
+          status: 400,
+          statusText: 'Bad Request',
+        },
+      ),
+    )
+  }
 
-	let json = parser.parse(xmlData);
+  let xmlData = await fetcher(rss_url)
+
+  let json = parser.parse(xmlData)
 
   return new Response(JSON.stringify(json.rss.channel), {
     headers: {
-        "content-type": "application/json;charset=UTF-8"
-      }
+      'content-type': 'application/json;charset=UTF-8',
+    },
   })
 }
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request))
+})
