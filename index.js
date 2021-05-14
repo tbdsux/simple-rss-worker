00eb2fef83c6944e7407d48542447ec3f2a82ae3
TheBoringDude <iamcoderx@gmail.com>
2021-05-14 22:16:02 +0800

    access-control-allow-methods

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const res = (response, init = {}) => {
     headers: {
       'content-type': 'application/json;charset=UTF-8',
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST',
     },
     ...init,
   })
